3bf04dbfdb5c45df9757c1d7dd9925744b03ddfd
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-10 14:37:01 +0200

    7e commit - TP7

---
+++ src/fr/diginamic/banque/entites/CompteTaux.java
@@ -0,0 +1,24 @@
+package fr.diginamic.banque.entites;
+
+public class CompteTaux extends Compte{
+
+	private float tauxRemun;
+
+	public CompteTaux(int numeroCompte, float soldeCompte, float tauxRemun) {
+		super(numeroCompte, soldeCompte);
+		this.tauxRemun = tauxRemun;
+	}
+
+	public float getTauxRemun() {
+		return tauxRemun;
+	}
+
+	public void setTauxRemun(float tauxRemun) {
+		this.tauxRemun = tauxRemun;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " / CompteTaux [tauxRemun=" + tauxRemun + "]";
+	}
+}
--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -6,6 +6,17 @@ public class TestBanque {
 		
 		Compte monCompte = new Compte(15348677, 1200.65f);	
 		System.out.println(monCompte.toString());
+		System.out.println();
+		
+		Compte[] myTab = new Compte[2];
+		CompteTaux monCompteTaux = new CompteTaux (875633134, 8530.52f, 1.25f);
+		
+		myTab[0] = monCompte;
+		myTab[1] = monCompteTaux;
+		
+		for (int i=0; i<myTab.length; i++) {
+			System.out.println(i + " : " + myTab[i].toString());
+		}
 	}
 
 }
