8023af2b79ee4870042e677f7a51700770b04acd
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-17 10:43:43 +0200

    22e commit - Exo LireFichier fixes TP 15 Fichiers

---
+++ src/maps/CreationMap.java
@@ -0,0 +1,35 @@
+package maps;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class CreationMap {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, Double> mapSalaires = new HashMap<>();
+		
+		mapSalaires.put("Paul", 1750d);
+		mapSalaires.put("Hicham", 1825d);
+		mapSalaires.put("Yu", 2250d);
+		mapSalaires.put("Ingrid", 2015d);
+		mapSalaires.put("Chantal", 2418d);
+		
+		System.out.println("\nListe initiale :\n");
+		Iterator<Double> iter = mapSalaires.values().iterator();
+		while (iter.hasNext()) {
+			Double value = iter.next();
+			//String name = iter.toString();
+			System.out.println(value);
+		}
+		
+		for (Double valDouble : mapSalaires.values()) {
+			
+		}
+		
+		for (String valString : mapSalaires.keySet()) {
+			
+		}
+	}
+
+}
