5492fd04dd31b2b37be073cefeac76fc22106bd9
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-17 11:26:24 +0200

    24e commit - Exo LireFichierAvecInstanciation TP 15 Fichiers

--- src/fichier/LectureFichier.java
+++ src/fichier/LectureFichier.java
@@ -5,17 +5,32 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LectureFichier {
 
 	public static void main(String[] args) throws IOException{
 		
+		ArrayList<Ville> listeVilles = new ArrayList<>();
 		Path path = Paths.get("/home/jacques/springToolsSuite/workSTS/approche-objet/src/fichier/files/recensement.csv");
 		
 		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+		lines.remove(0);
 		for (String line: lines) {
-			System.out.println(line);
+			String[] tabOneLine = line.split(";");
+			
+			
+			//System.out.println("nom : " + tabOneLine[6] + " dep : " + tabOneLine[2] + " reg : " + tabOneLine[1] + " pop : " + tabOneLine[9].replace(" ", ""));
+			Ville ville = new Ville(tabOneLine[6], tabOneLine[2], tabOneLine[1], Integer.parseInt(tabOneLine[9].replace(" ", "")));
+			listeVilles.add(ville);
+			//System.out.println(ville.toString());
+		}
+		
+		System.out.println("\n\n");
+		
+		for (Ville ville : listeVilles) {
+			System.out.println(ville.toString());
 		}
 
 	}
--- src/fichier/Ville.java
+++ src/fichier/Ville.java
@@ -1,5 +1,55 @@
 package fichier;
 
 public class Ville {
+	private String Nom;
+	private String CodeDep;
+	private String nomRegion;
+    private long popTotale;
+    
+	public Ville(String nom, String codeDep, String nomRegion, long popTotale) {
+		super();
+		this.Nom = nom;
+		this.CodeDep = codeDep;
+		this.nomRegion = nomRegion;
+		this.popTotale = popTotale;
+	}
 
+	public String getNom() {
+		return Nom;
+	}
+
+	public void setNom(String nom) {
+		this.Nom = nom;
+	}
+
+	public String getCodeDep() {
+		return CodeDep;
+	}
+
+	public void setCodeDep(String codeDep) {
+		this.CodeDep = codeDep;
+	}
+
+	public String getNomRegion() {
+		return nomRegion;
+	}
+
+	public void setNomRegion(String nomRegion) {
+		this.nomRegion = nomRegion;
+	}
+
+	public long getPopTotale() {
+		return popTotale;
+	}
+
+	public void setPopTotale(long popTotale) {
+		this.popTotale = popTotale;
+	}
+
+	@Override
+	public String toString() {
+		return "Nom : " + this.Nom + ", CodeDep : " + this.CodeDep + ", nomRegion : " + this.nomRegion + ", popTotale : " + this.popTotale;
+	}
+    
+    
 }
