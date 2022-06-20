d301ab362eb31a32300f3ef7fad3a3085dd69a0f
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-20 11:40:39 +0200

    57e commit - réparation .gitignore

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.diginamic.recensement"},{"l":"fr.diginamic.recensement.controller"},{"l":"fr.diginamic.recensement.model"},{"l":"fr.diginamic.recensement.vue"}];updateSearchResults();
