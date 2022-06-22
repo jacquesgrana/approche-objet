8717f5c4dd2881c3e16f33af20a87bba0e091a29
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-22 14:55:40 +0200

    74e commit - TP StringBuilder

---
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -0,0 +1,27 @@
+package fr.diginamic.chaines;
+
+public class TestStringBuilder {
+
+	public static void main(String[] args) {
+		long debut = System.currentTimeMillis();
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i=1; i<= 100000; i++) {
+			stringBuilder.append(i).append("/");
+		}
+		long fin = System.currentTimeMillis();
+		
+		System.out.println("Temps avec stringBuilder : " + (fin - debut));
+		
+		String string = "";
+		long debut1 = System.currentTimeMillis();
+		for (int i=1; i<= 100000; i++) {
+			string += i + "/";
+		}
+		long fin1 = System.currentTimeMillis();
+		
+		System.out.println("\nTemps avec stringBuilder : " + (fin1 - debut1));
+
+	}
+
+}
