72a7db2255bb6ca1c4594c1e0cdbefc152372a94
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-22 10:22:04 +0200

    69e commit - TP 12 - Exo 3 refactor

--- src/maps/FusionMap.java
+++ src/maps/FusionMap.java
@@ -9,19 +9,19 @@ public class FusionMap {
 	public static void main(String[] args) {
 		
 		// Création map1
-		HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+		HashMap<Integer, String> map1 = new HashMap<>();
 		map1.put(1, "Rouge");
 		map1.put(2, "Vert");
 		map1.put(3, "Orange");
 		// Création map2
-		HashMap<Integer, String> map2 = new HashMap<Integer, String>();
+		HashMap<Integer, String> map2 = new HashMap<>();
 		map2.put(4, "Blanc");
 		map2.put(5, "Bleu");
 		map2.put(6, "Orange");
 		
 		
-		HashMap<Integer, String> map3 = new HashMap<Integer, String>();
-		
+		HashMap<Integer, String> map3 = new HashMap<>();
+		/*
 		Iterator<Entry<Integer, String>> iter1 = map1.entrySet().iterator();
 		while(iter1.hasNext()) {
 			String value = (String) iter1.next().toString();
@@ -38,13 +38,27 @@ public class FusionMap {
 			//System.out.println("value : key : " + extracted[0] + " valeur : " + extracted[1]);
 			
 			map3.put(Integer.valueOf(extracted[0]), extracted[1]);
+		}*/
+		
+		Iterator<Integer> iter1 = map1.keySet().iterator();
+		while(iter1.hasNext()) {
+			Integer key = iter1.next();
+			String value = map1.get(key);
+			map3.put(key, value);
+		}
+		
+		Iterator<Integer> iter2 = map2.keySet().iterator();
+		while(iter2.hasNext()) {
+			Integer key = iter2.next();
+			String value = map2.get(key);
+			map3.put(key, value);
 		}
 		
 		System.out.println("\nListe :\n");
-		Iterator<Entry<Integer, String>> iter3 = map3.entrySet().iterator();
+		Iterator<Integer> iter3 = map3.keySet().iterator();
 		while(iter3.hasNext()) {
-			String value = (String) iter3.next().toString();
-			System.out.println(value);
+			Integer key = iter3.next();
+			System.out.println("key : " + key + " / value : " + map3.get(key));
 		}
 		
 	}
