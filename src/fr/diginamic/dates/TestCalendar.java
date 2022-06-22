d54369115b292cada85171d57aa43e6a5ef6c553
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-22 17:06:05 +0200

    77e commit - TP Date et Calendar - refactor

--- src/fr/diginamic/dates/TestCalendar.java
+++ src/fr/diginamic/dates/TestCalendar.java
@@ -10,34 +10,34 @@ public class TestCalendar {
 	public static void main(String[] args) {
 		
 
-        SimpleDateFormat dtf2 = new SimpleDateFormat("dd/MM/yyyy");
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.set(2016, 4, 19, 23, 59, 30);
-        Date dateObj2 = calendar2.getTime();
-        String formattedDate2 = dtf2.format(dateObj2);
-        System.out.println("\nObjet date : " + formattedDate2);
-		
-		SimpleDateFormat dtf1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat dtfSimple = new SimpleDateFormat("dd/MM/yyyy");
         Calendar calendar1 = Calendar.getInstance();
-        Date dateObj1 = calendar1.getTime();
-        String formattedDate1 = dtf1.format(dateObj1);
-        System.out.println("\nDate du jour : " + formattedDate1);
+        calendar1.set(2016, 4, 19, 23, 59, 30);
+        Date dateDefinie = calendar1.getTime();
+        String formattedDateDefinie = dtfSimple.format(dateDefinie);
+        System.out.println("\nObjet date : " + formattedDateDefinie);
+		
+		SimpleDateFormat dtf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        Date dateDuJour = calendar.getTime();
+        String formattedDateduJour = dtf.format(dateDuJour);
+        System.out.println("\nDate du jour : " + formattedDateduJour);
         
-        SimpleDateFormat dtf3 = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.FRANCE);
-        String formattedDate3 = dtf3.format(dateObj1);
-        System.out.println("\nDate du jour en français : " + formattedDate3);
+        SimpleDateFormat dtfFrancais = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.FRANCE);
+        String formattedDateFrancais = dtfFrancais.format(dateDuJour);
+        System.out.println("\nDate du jour en français : " + formattedDateFrancais);
         
-        SimpleDateFormat dtf4 = new SimpleDateFormat("EEEE dd MMMM yyyy", new Locale("ru", "RU"));
-        String formattedDate4 = dtf4.format(dateObj1);
-        System.out.println("\nDate du jour en russe : " + formattedDate4);
+        SimpleDateFormat dtfRusse = new SimpleDateFormat("EEEE dd MMMM yyyy", new Locale("ru", "RU"));
+        String formattedDateRusse = dtfRusse.format(dateDuJour);
+        System.out.println("\nDate du jour en russe : " + formattedDateRusse);
         
-        SimpleDateFormat dtf5 = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.CHINA);
-        String formattedDate5 = dtf5.format(dateObj1);
-        System.out.println("\nDate du jour en chinois : " + formattedDate5);
+        SimpleDateFormat dtfChinois = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.CHINA);
+        String formattedDateChinois = dtfChinois.format(dateDuJour);
+        System.out.println("\nDate du jour en chinois : " + formattedDateChinois);
         
-        SimpleDateFormat dtf6 = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.GERMANY);
-        String formattedDate6 = dtf6.format(dateObj1);
-        System.out.println("\nDate du jour en allemand : " + formattedDate6);
+        SimpleDateFormat dtfAllemand = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.GERMANY);
+        String formattedDateAllemand = dtfAllemand.format(dateDuJour);
+        System.out.println("\nDate du jour en allemand : " + formattedDateAllemand);
 	}
 
 }
